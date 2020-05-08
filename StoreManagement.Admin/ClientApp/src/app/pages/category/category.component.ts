288e20c5266c41a47ce19a78e4bdc2f86a643149
QuangBao16T1 <bao16t1@gmail.com>
2020-05-08 17:08:37 +0700

    CategoryCommit

--- StoreManagement.Admin/ClientApp/src/app/pages/category/category.component.ts
+++ StoreManagement.Admin/ClientApp/src/app/pages/category/category.component.ts
@@ -1,14 +1,87 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, TemplateRef } from '@angular/core';
+import { Router } from '@angular/router';
+import { UserService } from 'src/app/services/user.service';
+import { User } from 'src/app/models/user/user.model';
+import { HttpErrorResponse } from '@angular/common/http';
+import { ToastrService } from 'ngx-toastr';
+import { Observable } from 'rxjs';
+import { map, tap } from 'rxjs/operators';
+import {  BsModalRef, BsModalService } from 'ngx-bootstrap/modal';
+import { CategoryService } from 'src/app/services/category.service';
+import { Category } from 'src/app/models/category/category.model';
 
 @Component({
-  selector: 'app-category',
-  templateUrl: './category.component.html'
+    selector: 'app-category',
+    templateUrl: './category.component.html'
 })
 export class CategoryComponent implements OnInit {
+    category: Category;
+    keyword: string;
+    itemsAsync: Observable<any[]>;
+    modalRef: BsModalRef;
 
-  constructor() { }
+    constructor(
+        // tslint:disable-next-line:no-shadowed-variable
+        public CategoryService: CategoryService,
+        private router: Router,
+        private modalService: BsModalService,
+        private toastr: ToastrService
+    ) { }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+        this.keyword = '';
+        this.getAllCategories();
+    }
 
+    getAllCategories() {
+        this.itemsAsync = this.CategoryService.getAllCategories(this.keyword);
+    }
+
+    add() {
+        this.router.navigate(['/categories/add']);
+    }
+
+    edit(id: any) {
+        this.router.navigate(['/categories/edit/' + id]);
+    }
+
+    // editFull(id: any) {
+    //     this.router.navigate(['/users/editfull/' + id]);
+    // }
+
+    deleteConfirm(template: TemplateRef<any>, data: any) {
+        this.category = Object.assign({}, data);
+        this.modalRef = this.modalService.show(template);
+    }
+
+    confirm(): void {
+        if (this.category) {
+            this.CategoryService.deleteCategory(this.category.id)
+                .subscribe(
+                    () => {
+                        this.getAllCategories();
+                        this.toastr.success(`Xóa loại hàng thành công`);
+                    },
+                    (error: HttpErrorResponse) => {
+                        this.toastr.error(('Xóa loại hàng không thành công'));
+                    }
+                );
+        }
+        this.category = undefined;
+        this.modalRef.hide();
+    }
+
+    close(): void {
+        this.category = undefined;
+        this.modalRef.hide();
+    }
+
+    search() {
+        this.getAllCategories();
+    }
+
+    refresh() {
+        this.keyword = '';
+        this.getAllCategories();
+    }
 }
