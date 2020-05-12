9b94997f33ca1d618addcf4a4cc2f46b1f28f9c0
NQLuan <nguyenquangluan02@gmail.com>
2020-05-12 14:14:42 +0700

    add SupplierUI.cs is deleted

---
+++ StoreManagement.BusinessLogic/Dtos/Suppliers/SupplierUI.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StoreManagement.BusinessLogic.Dtos.Suppliers
+{
+    class SupplierUI
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Address { get; set; }
+    }
+}
