fd9446a93d98cb57e8c7b9ca13151afa00d2c5d5
NQLuan <nguyenquangluan02@gmail.com>
2020-05-22 23:14:56 +0700

    Finish Order

--- StoreManagement.BusinessLogic/Dtos/OrderDetails/OrderDetailAdd.cs
+++ StoreManagement.BusinessLogic/Dtos/OrderDetails/OrderDetailAdd.cs
@@ -6,10 +6,8 @@ namespace StoreManagement.BusinessLogic.Dtos.OrderDetails
 {
     public class OrderDetailAdd
     {
-        public int Id { get; set; }
         public int Quantity { get; set; }
         public int DisCount { get; set; }
         public int ProductId { get; set; }
-        public int OrderId { get; set; }
     }
 }
